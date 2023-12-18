762917f47469bf9c418e5936de4e809e9175dc18
Duncan Miller <dmiller@heatspring.com>
2023-12-18 12:27:32 -0800

    define symbols

--- Section_02_knowledge/clue.py
+++ Section_02_knowledge/clue.py
@@ -1,2 +1,18 @@
 from logic import *
 
+mustard = Symbol("ColMustard")
+plum = Symbol("ProfPlum")
+scarlet = Symbol("MsScarlet")
+characters = [mustard, plum, scarlet]
+
+ballroom = Symbol("ballroom")
+kitchen = Symbol("kitchen")
+library = Symbol("library")
+rooms = [ballroom, kitchen, library]
+
+knife = Symbol("knife")
+revolver = Symbol("revolver")
+wrench = Symbol("wrench")
+weapons = [knife, revolver, wrench]
+
+symbols = characters + rooms + weapons
