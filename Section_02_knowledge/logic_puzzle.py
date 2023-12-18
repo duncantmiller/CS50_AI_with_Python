53a081dbc03ffbf04fb4a0897344bf7b4163904d
Duncan Miller <dmiller@heatspring.com>
2023-12-18 12:42:02 -0800

    start logic puzzle

---
+++ Section_02_knowledge/logic_puzzle.py
@@ -0,0 +1,7 @@
+# gilderoy, minerva, pomona and horace each belong to a different on of the four houses: gryffindor, hufflepuff, ravenclaw and slytherin
+# gilderoy belongs to gryffindor or ravenclaw
+# pomona does not belong in slytherin
+# minerva belongs to gryffindor
+
+from logic import *
+
