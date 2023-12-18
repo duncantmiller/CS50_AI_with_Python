7e72ef7f4b44cbd7e0848c3e378c7a9462a5d68d
Duncan Miller <dmiller@heatspring.com>
2023-12-18 12:07:16 -0800

    create knowledge base

---
+++ Section_02_knowledge/harry_potter.py
@@ -0,0 +1,17 @@
+from logic import *
+
+# If it is not raining, then Harry visited Hagrid
+# Harry either visited Hagrid or Dumbledore
+# Harry did not visit both
+# Harry visited Dumbledore
+
+harry = Symbol("harry")
+hagrid = Symbol("hagrid") # Harry visited Hagrid
+dumbledore = Symbol("dumbledore") # Harry visited Dumbledore
+
+knowledge = And(
+    Implication(Not(rain), hagrid),
+    Or(hagrid, dumbledore),
+    Not(And(hagrid, dumbledore)),
+    dumbledore
+)
