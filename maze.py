a582fdbd61cf992770ffda90db1da11746f5d484
Duncan Miller <dmiller@heatspring.com>
2023-12-18 10:14:36 -0800

    start exploration loop

--- maze.py
+++ maze.py
@@ -53,3 +53,34 @@ class Maze():
         frontier.add(start)
 
         self.explored = set()
+
+        while True:
+
+            if frontier.empty():
+                raise Exception("no solution")
+
+            node = frontier.remove()
+            self.num_explored += 1
+
+            if node.state == self.goal:
+                actions = []
+                cells = []
+
+                while node.parent is not None:
+                    actions.append(node.action)
+                    cells.append(node.state)
+                    node = node.parent
+                actions.reverse()
+                cells.reverse()
+                self.solution = (actions, cells)
+                return
+
+            self.explored.add(node.state)
+
+            for action, state in self.neighbors(node.state):
+                if not frontier.contains_state(state) and state not in self.explored:
+                    child = Node(state=state, parent=node, action=action)
+                    frontier.add(child)
+
+
+
