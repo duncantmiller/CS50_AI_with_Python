1affd8faefdf9593c6869e4d5922381e9f0e3d0e
Duncan Miller <dmiller@heatspring.com>
2023-12-18 21:03:39 -0800

    removing unecessary conditional

--- Section_01_search/degrees/degrees.py
+++ Section_01_search/degrees/degrees.py
@@ -102,10 +102,9 @@ def shortest_path(source, target):
     for source_movie_id in source_movie_ids:
         stars = movies[source_movie_id]["stars"]
         for star_id in stars:
-            if star_id != source_id:
-                for star_movie_id in people[star_id]["movies"]:
-                    if star_movie_id in target_movie_ids:
-                        return [(source_movie_id, star_id), (star_movie_id, target_id)]
+            for star_movie_id in people[star_id]["movies"]:
+                if star_movie_id in target_movie_ids:
+                    return [(source_movie_id, star_id), (star_movie_id, target_id)]
 
 
 def person_id_for_name(name):
