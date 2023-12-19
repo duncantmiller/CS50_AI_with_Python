79b7bb03e96cc496a51f2449e9d507af5ae8d810
Duncan Miller <dmiller@heatspring.com>
2023-12-19 12:08:21 -0800

    utility passes first test

--- Section_01_search/tictactoe/tictactoe.py
+++ Section_01_search/tictactoe/tictactoe.py
@@ -124,7 +124,10 @@ def utility(board):
     """
     Returns 1 if X has won the game, -1 if O has won, 0 otherwise.
     """
-    raise NotImplementedError
+    score = 0
+    if winner(board) == "X":
+        score = 1
+    return score
 
 
 def minimax(board):
